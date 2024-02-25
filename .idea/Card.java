5ab8e94d51fcf0c4fe8dd5a67f1353715ccf040d
tanmayhaldiya <tanmayhaldiya@gmail.com>
2024-02-25 14:24:44 -0500

    added DeckOfCards and Card

---
+++ .idea/Card.java
@@ -0,0 +1,73 @@
+/* -----------------------------------------------------------
+   Encoding:
+
+        Suit: 4 = Spade
+              3 = Heart
+              2 = Club
+              1 = Diamond
+
+        Rank:  A = 1
+               2 = 2
+               ...
+               J = 11
+               Q = 12
+               K = 13
+
+   Card:
+
+         byte cardSuit;                -- contain 1, 2, 3, or 4
+         byte cardRank;                -- contain 2, 3, ... 13, 14
+   ----------------------------------------------------------- */
+
+public class Card
+{
+    public static final int SPADE   = 4;
+    public static final int HEART   = 3;
+    public static final int CLUB    = 2;
+    public static final int DIAMOND = 1;
+
+    private static final String[] Suit = { "*", "d", "c", "h", "s"};
+    private static final String[] Rank = { "*", "*", "2", "3", "4",
+            "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+
+    private byte cardSuit;
+    private byte cardRank;
+
+    public Card( int suit, int rank )
+    {
+        if ( rank == 1 )
+            cardRank = 14;     // Give Ace the rank 14
+        else
+            cardRank = (byte) rank;
+
+        cardSuit = (byte) suit;
+    }
+
+    public int suit()
+    {
+        return ( cardSuit );         // This is a shorthand for:
+        //   this.cardSuit
+    }
+
+    public String suitStr()
+    {
+        return( Suit[ cardSuit ] );  // This is a shorthand for:
+        //   this.Suit[ this.cardSuit ]
+    }
+
+    public int rank()
+    {
+        return ( cardRank );
+    }
+
+    public String rankStr()
+    {
+        return ( Rank[ cardRank ] );
+    }
+
+
+    public String toString()
+    {
+        return ( Rank[ cardRank ] + Suit[ cardSuit ] );
+    }
+}
---
+++ .idea/DeckOfCards.java
@@ -0,0 +1,80 @@
+
+/* -----------------------------------------------------
+   Deck: a deck of cards
+   ----------------------------------------------------- */
+
+public class DeckOfCards
+{
+    public static final int NCARDS = 52;
+
+    private Card[] deckOfCards;         // Contains all 52 cards
+    private int currentCard;            // deal THIS card in deck
+
+    public DeckOfCards( )
+    {
+        deckOfCards = new Card[ NCARDS ];
+
+        int i = 0;
+
+        for ( int suit = Card.DIAMOND; suit <= Card.SPADE; suit++ )
+            for ( int rank = 1; rank <= 13; rank++ )
+                deckOfCards[i++] = new Card(suit, rank);
+
+        currentCard = 0;
+    }
+
+    /* ---------------------------------
+       shuffle(n): shuffle the deck
+       --------------------------------- */
+    public void shuffle(int n)
+    {
+        int i, j, k;
+
+        for ( k = 0; k < n; k++ )
+        {
+            i = (int) ( NCARDS * Math.random() );  // Pick 2 random cards
+            j = (int) ( NCARDS * Math.random() );  // in the deck
+
+	  /* ---------------------------------
+	     swap these randomly picked cards
+	     --------------------------------- */
+            Card tmp = deckOfCards[i];
+            deckOfCards[i] = deckOfCards[j];
+            deckOfCards[j] = tmp;
+        }
+
+        currentCard = 0;   // Reset current card to deal
+    }
+
+    /* -------------------------------------------
+       deal(): deal deckOfCards[currentCard] out
+       ------------------------------------------- */
+    public Card deal()
+    {
+        if ( currentCard < NCARDS )
+        {
+            return ( deckOfCards[ currentCard++ ] );
+        }
+        else
+        {
+            System.out.println("Out of cards error");
+            return ( null );  // Error;
+        }
+    }
+
+    public String toString()
+    {
+        String s = "";
+        int k;
+
+        k = 0;
+        for ( int i = 0; i < 4; i++ )
+        {
+            for ( int j = 1; j <= 13; j++ )
+                s += (deckOfCards[k++] + " ");
+
+            s += "\n";
+        }
+        return ( s );
+    }
+}
