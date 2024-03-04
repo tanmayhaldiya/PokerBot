7dc483edd2542ca9744aef4e31f8c8c4ebaab61b
LegendaryDK <55328687+LegendaryDK@users.noreply.github.com>
2024-03-03 22:53:17 -0500

    added new string constructor to Card

--- src/Card.java
+++ src/Card.java
@@ -27,14 +27,14 @@ public class Card
     public static final int CLUB    = 2;
     public static final int DIAMOND = 1;
 
-    private static final String[] Suit = { "*", "d", "c", "h", "s"};
-    private static final String[] Rank = { "*", "*", "2", "3", "4",
-            "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+    private static final String[] Suit = { "*", "d", "c", "h", "s"};  // "*" is so that we start at index 1
+    private static final String[] Rank = { "*", "*", "2", "3", "4",  // "*" "*" is so that we start at index 2
+            "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"};
 
     private byte cardSuit;
     private byte cardRank;
 
-    public Card( int suit, int rank )
+    public Card(int suit, int rank )
     {
         if ( rank == 1 )
             cardRank = 14;     // Give Ace the rank 14
@@ -42,6 +42,38 @@ public class Card
             cardRank = (byte) rank;
 
         cardSuit = (byte) suit;
+    }
+
+    public Card(String cardStr) 
+    {
+        char rank = cardStr.charAt(0);
+        char suit = cardStr.charAt(1);
+        if (rank >= 50 && rank <= 57) {
+            cardRank = (byte) (rank - 48);
+        } else if (rank == 'T') {
+            cardRank = 10;
+        } else if (rank == 'J') {
+            cardRank = 11;
+        } else if (rank == 'Q') {
+            cardRank = 12;
+        } else if (rank == 'K') {
+            cardRank = 13;
+        } else if (rank == 'A') {
+            cardRank = 14;
+        } else {
+            System.out.println("Invalid rank input");
+        }
+        if (suit == 'd' || suit == 'D') {
+            cardSuit = 1;
+        } else if (suit == 'c' || suit == 'C') {
+            cardSuit = 2;
+        } else if (suit == 'h' || suit == 'H') {
+            cardSuit = 3;
+        } else if (suit == 's' || suit == 'S') {
+            cardSuit = 4;
+        } else {
+            System.out.println("Invalid suit input");
+        }
     }
 
     public int suit()
